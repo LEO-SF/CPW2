80a91271968a9469674ecef2f4343abb1839b907
LEO-SF <71852506+LEO-SF@users.noreply.github.com>
2023-08-31 20:31:56 -0300

    Update index.js
    Testes passando

--- index.js
+++ index.js
@@ -5,7 +5,14 @@
  * @param {*} people : Um array contendo um conjunto de nomes
  * @returns Um array com os nomes invertidos
  */
-function invert(people){
+function invert(people) {
+
+    let nomes = []
+
+    for (let i = people.length - 1; i >= 0; i--) {
+        console.log(people[i]);
+        nomes.push(people[i]);
+    }
 
     // TODO
     // 1) Peça ao usuário para digitar vários nomes. Exiba na tela
@@ -13,7 +20,7 @@ function invert(people){
     // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
     // nomes dos usuários.
 
-    return [];
+    return nomes;
 }
 
 /**
@@ -22,14 +29,23 @@ function invert(people){
  * @param {*} grades : Um array de notas
  * @returns Uma média a partir do array de notas
  */
-function mean(grades){
+function mean(grades) {
+
+
+    let notas = 0;
+
+    for (let i = 0; i < grades.length; i++) {
+        notas += grades[i];
+    }
+
+    notas = notas / 3;
 
     // TODO
     // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
     // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
     // ou superior a 7.
 
-    return 0;
+    return notas;
 }
 
 /**
@@ -38,14 +54,34 @@ function mean(grades){
  * @param {*} mean : Um número que representa uma média
  * @returns Uma string dizendo se um estudante está aprovado ou não
  */
-function isApproved(mean){
+function isApproved(mean) {
+
+    let notas = mean;
+
+    if (mean.isArray) {
+
+        notas = 0;
+
+        for (let i = 0; i < mean.length; i++) {
+            notas += mean[i];
+        }
+        notas = notas / 3;
+    }
+
+    let str = "";
+
+    if (notas >= 7) {
+        str = "aprovado";
+    } else if (notas < 7) {
+        str = "reprovado";
+    }
 
     // TODO
     // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
     // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
     // ou superior a 7.
 
-    return "";
+    return str;
 }
 
 /**
@@ -54,7 +90,21 @@ function isApproved(mean){
  * @param {*} strDate : Uma string no formato de data
  * @returns Uma descrição da data informada
  */
-function wide(strDate){
+function wide(strDate) {
+
+    if (/^\d{2}-\d{2}-\d{4}$/.test(strDate)) {
+        strDate = "";
+    }
+
+    if (strDate !== "") {
+        let meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro",
+            "novembro", "dezembro"];
+        let dia = strDate.split("/")[0];
+        let mes = parseInt(strDate.split("/")[1]);
+        mes = meses[mes - 1];
+        let ano = strDate.split("/")[2];
+        strDate = dia + " de " + mes + " de " + ano;
+    }
 
     // TODO
     // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
@@ -62,7 +112,7 @@ function wide(strDate){
     // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
     // Nesse caso, o separador é a barra (/) da data.
 
-    return "";
+    return strDate;
 }
 
 // Trecho utilizado pelos testes
